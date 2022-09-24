4eaef863707fbf9323d1a8894f92073a67c837ea
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-09-24 13:57:17 +0600

    Material UI Add

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
+import { Fragment } from "react";
+import NavBar from "./components/NavBar";
+import ResponsiveAppBar from "./components/NavBar2";
+
 function App() {
   return (
-    <div className="d-flex justify-content-center align-items-center vh-100 text-center">
-      <div className="text">
-        <h2 className="text-shadow">SR Blood</h2>
-        <br />
-        <h1>Under Constraction</h1>
-        <br />
-      </div>
-    </div>
+    <Fragment>
+      <NavBar />
+      <ResponsiveAppBar />
+    </Fragment>
   );
 }
 
