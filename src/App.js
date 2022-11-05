62cb7603a37764cd051f4fedb1bfc280fb942b72
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2022-11-05 14:23:30 +0600

    Tailwind setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import Header from "./components/Header";
 
 function App() {
-  return <Header />;
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
 }
 
 export default App;
