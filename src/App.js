2ae8d1a71ea7d3a3fa0dd27a01e50381d225eabd
Borhan Uddin <68281712+wapborhan@users.noreply.github.com>
2023-02-15 21:19:45 +0600

    Update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import Header from "./components/header/Header";
 import Banner from "./home/banner";
+import About from "./home/about";
+import Causes from "./home/causes";
 
 function App() {
   return (
     <div class="page-wrapper">
+      {" "}
+      <div class="custom-cursor__cursor"></div>
+      <div class="custom-cursor__cursor-two"></div>
       <Header />
       {/* <Banner /> */}
+      <About />
+      <Causes />
     </div>
   );
 }
---
+++ src/home/about/index.jsx
@@ -0,0 +1,122 @@
+import React from "react";
+
+const index = () => {
+  return (
+    <section className="about-one">
+      <div className="about-one__shape-box-1">
+        <div
+          className="about-one__shape-1"
+          // style="background-image: url(assets/images/shapes/about-one-shape-1.png);"
+        ></div>
+      </div>
+      <div className="container">
+        <div className="row">
+          <div className="col-xl-6">
+            <div className="about-one__left">
+              <div
+                className="about-one__img-box wow slideInLeft"
+                data-wow-delay="100ms"
+                data-wow-duration="2500ms"
+              >
+                <div className="about-one__img">
+                  <img
+                    src="assets/images/resources/about-one-img-1.jpg"
+                    alt=""
+                  />
+                </div>
+                <div className="about-one__img-border"></div>
+                <div className="about-one__curved-circle-box">
+                  <div className="curved-circle">
+                    <span className="curved-circle--item">
+                      25 Years Experience oxpins Charity Center
+                    </span>
+                  </div>
+
+                  <div className="about-one__curved-circle-icon">
+                    <img
+                      src="assets/images/icon/curved-circle-icon.png"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="about-one__shape-2 zoom-fade">
+                  <img
+                    src="assets/images/shapes/about-one-shape-2.png"
+                    alt=""
+                  />
+                </div>
+                <div className="about-one__shape-3 float-bob-y">
+                  <img
+                    src="assets/images/shapes/about-one-shape-3-dark.png"
+                    alt=""
+                  />
+                </div>
+                <div className="about-one__shape-4 zoominout">
+                  <img
+                    src="assets/images/shapes/about-one-shape-4.png"
+                    alt=""
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-xl-6">
+            <div className="about-one__right">
+              <div className="section-title text-left">
+                <span className="section-title__tagline">
+                  Welcome to oxpins charity
+                </span>
+                <h2 className="section-title__title">
+                  Helping each other can make world better
+                </h2>
+              </div>
+              <p className="about-one__text">
+                We help companies develop powerful corporate social
+                responsibility, grantmaking, and employee engagement strategies.
+                Our impact is about more than moving money to where it’s needed
+                most it’s also about helping nonprofits access information and
+                ideas that will help them listen, learn, and grow.{" "}
+              </p>
+              <div className="about-one__fund">
+                <p className="about-one__fund-text">
+                  Helped fund <span>24,537</span> Projects in
+                  <span>24</span> Countries, Benefiting over <br />{" "}
+                  <span>8.2</span> Million people.
+                </p>
+              </div>
+              <ul className="list-unstyled about-one__points">
+                <li>
+                  <div className="icon">
+                    <span className="icon-volunteer"></span>
+                  </div>
+                  <div className="text">
+                    <h5>
+                      <a href="become-volunteer.html">Join our team</a>
+                    </h5>
+                    <p>Lorem ipsum dolor sit amet not quis mis notted.</p>
+                  </div>
+                </li>
+                <li>
+                  <div className="icon">
+                    <span className="icon-solidarity"></span>
+                  </div>
+                  <div className="text">
+                    <h5>
+                      <a href="donate-now.html">Start donating</a>
+                    </h5>
+                    <p>Lorem ipsum dolor sit amet not quis mis notted.</p>
+                  </div>
+                </li>
+              </ul>
+              <a href="about.html" className="thm-btn about-one__btn">
+                Discover More
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default index;
